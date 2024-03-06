6517f2f83ac0b3a116fb3cedb4a36bdad28970d8
invincibleaayu <giridip862101@gmail.com>
2024-03-06 11:53:51 +0545

    feat: added config path

--- plugin_implementation_marker/hookimp.py
+++ plugin_implementation_marker/hookimp.py
@@ -2,21 +2,4 @@ import pluggy
 
 from constants import PACKAGE_NAME
 
-hookspec = pluggy.HookspecMarker(PACKAGE_NAME)
-
-
-class BaseHookSpecifications:
-    def __init__(self) -> None:
-        pass
-
-    @hookspec
-    async def sync_contact(args):
-        ...
-
-    @hookspec
-    async def sync_call_log(args):
-        ...
-
-    @hookspec
-    async def manage_call(args):
-        ...
+hookimpl = pluggy.HookimplMarker(PACKAGE_NAME)
